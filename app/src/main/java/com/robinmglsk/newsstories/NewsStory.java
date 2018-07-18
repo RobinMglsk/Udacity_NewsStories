d17977d828170e295729629880d041f446da4bbd
RobinMglsk <robin@migalski.be>
2018-07-18 22:43:52 +0200

    Added NewsStory Class

---
+++ app/src/main/java/com/robinmglsk/newsstories/NewsStory.java
@@ -0,0 +1,43 @@
+package com.robinmglsk.newsstories;
+
+public class NewsStory {
+
+    private String title;
+    private String sectionName;
+    private String url;
+    private String auhtor;
+    private long publishedAt;
+
+    public NewsStory(String title, String sectionName, String url){
+        this.title = title;
+        this.sectionName = sectionName;
+        this.url = url;
+    }
+
+    public NewsStory(String title, String sectionName, String url, String auhtor, long publishedAt){
+        this.title = title;
+        this.sectionName = sectionName;
+        this.url = url;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getSectionName(){
+        return this.sectionName;
+    }
+
+    public String getUrl(){
+        return this.url;
+    }
+
+    public String getAuhtor(){
+        return this.auhtor;
+    }
+
+    public long getPublishedAt(){
+        return this.publishedAt;
+    }
+
+}
