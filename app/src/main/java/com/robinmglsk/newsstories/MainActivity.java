fa21a9a76a7079f28e9a183c4f63f87b1dbc92e2
RobinMglsk <robin@migalski.be>
2018-07-18 23:29:13 +0200

    Added list, listAdapter and layout

--- app/src/main/java/com/robinmglsk/newsstories/MainActivity.java
+++ app/src/main/java/com/robinmglsk/newsstories/MainActivity.java
@@ -2,14 +2,23 @@ package com.robinmglsk.newsstories;
 
 import android.app.LoaderManager;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.net.URI;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NewsStoryAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,35 @@ public class MainActivity extends AppCompatActivity {
 
         checkNetworkConnection();
 
+        /** Setup custom adapter */
+        ListView newsStoryListView = (ListView) findViewById(R.id.list);
+        mAdapter = new NewsStoryAdapter(this, new ArrayList<NewsStory>());
+        newsStoryListView.setAdapter(mAdapter);
+        newsStoryListView.setEmptyView(findViewById(R.id.empty_list));
+
+        mAdapter.add(new NewsStory("Title","qsdsqd","https://www.robinmglsk.com"));
+
+
+        /**
+         * Set an item click listener on the ListView, which sends an intent to a web browser
+         * to open a website with more information about the selected news story.
+         */
+        newsStoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                /** Find the current news story that was clicked on */
+                NewsStory currentNewsStory = mAdapter.getItem(position);
+
+                /** Convert the String URL into a URI object (to pass into the Intent constructor) */
+                Uri newStoryUrl = Uri.parse(currentNewsStory.getUrl());
+
+                /** Create a new intent to view the news story URI */
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newStoryUrl);
+
+                /** Send the intent to launch a new activity */
+                startActivity(websiteIntent);
+            }
+        });
     }
 
     private void checkNetworkConnection(){
---
+++ app/src/main/java/com/robinmglsk/newsstories/NewsStoryAdapter.java
@@ -0,0 +1,87 @@
+package com.robinmglsk.newsstories;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class NewsStoryAdapter extends ArrayAdapter<NewsStory> {
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context     The current context. Used to inflate the layout file.
+     * @param newsStories A List of NewsStory objects to display in a list
+     */
+    public NewsStoryAdapter(Activity context, ArrayList<NewsStory> newsStories){
+        super(context,0, newsStories);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        /** Check if the existing view is being reused, otherwise inflate the view */
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.news_item, parent, false);
+        }
+
+        /** Get the {@link AndroidFlavor} object located at this position in the list */
+        NewsStory currentNewsStory = getItem(position);
+
+        /** Set title */
+        String title = currentNewsStory.getTitle();
+        TextView titleView = (TextView) listItemView.findViewById(R.id.title);
+        titleView.setText(title);
+
+        /** Set section */
+        String section = currentNewsStory.getSectionName();
+        TextView sectionView = (TextView) listItemView.findViewById(R.id.section);
+        sectionView.setText(section);
+
+        /** Set author */
+        String author = currentNewsStory.getAuhtor();
+        TextView authorView = (TextView) listItemView.findViewById(R.id.author);
+        if(author != null){
+            authorView.setText(author);
+        }else{
+            authorView.setText("");
+        }
+        sectionView.setText(title);
+
+        /** Set publish time */
+        Long publishedAt = currentNewsStory.getPublishedAt();
+        TextView publishedAtView = (TextView) listItemView.findViewById(R.id.publishedAt);
+        if(publishedAt != 0){
+            Date dateObject = new Date(publishedAt);
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("LLL dd, yyyy HH:mm");
+            String dateString = dateFormatter.format(dateObject);
+            publishedAtView.setText(dateString);
+        }else{
+            publishedAtView.setText("");
+        }
+
+
+        return listItemView;
+
+    }
+
+}
